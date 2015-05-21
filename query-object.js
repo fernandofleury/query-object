138295793f71aa73ae0ff1ccf6b45e3cda647f24
James Brooks <james@bluebaytravel.co.uk>
2015-05-21 09:52:41 +0100

    Closes #10 - Supporting of nested arrays

--- query-object.js
+++ query-object.js
@@ -97,19 +97,9 @@
       return;
     }
 
-    var query = '',
+    var query = _self.objectToQueryString(obj),
       href = location.href,
       prop;
-
-    for (prop in obj) {
-      query += encodeURIComponent(prop);
-      if (obj[prop]) {
-        query += '=' + encodeURIComponent(obj[prop]);
-      }
-      query += '&';
-    }
-
-    query = query.slice(0, -1);
 
     if (_self.useHistory) {
       href = href.indexOf('?') ? href.substr(0, href.indexOf('?')) : href;
@@ -175,6 +165,54 @@
     }
     var query = _self.get() || {};
     return query.hasOwnProperty(prop);
+  };
+
+  _self.objectToQueryString = function (a) {
+    var prefix, s, add, name, r20, output;
+    s = [];
+    r20 = /%20/g;
+    add = function (key, value) {
+      // If value is a function, invoke it and return its value
+      value = ( typeof value == 'function' ) ? value() : ( value == null ? "" : value );
+      if (value) {
+        s[ s.length ] = encodeURIComponent(key) + "=" + encodeURIComponent(value);
+      } else {
+        s[ s.length ] = encodeURIComponent(key);
+      }
+    };
+    if (a instanceof Array) {
+      for (name in a) {
+        add(name, a[name]);
+      }
+    } else {
+      for (prefix in a) {
+        _self.buildParams(prefix, a[ prefix ], add);
+      }
+    }
+    output = s.join("&").replace(r20, "+");
+    return output;
+  };
+
+  _self.buildParams = function(prefix, obj, add) {
+    var name, i, l, rbracket;
+    rbracket = /\[\]$/;
+    if (obj instanceof Array) {
+      for (i = 0, l = obj.length; i < l; i++) {
+        if (rbracket.test(prefix)) {
+          add(prefix, obj[i]);
+        } else {
+          _self.buildParams(prefix + "[" + ( typeof obj[i] === "object" ? i : "" ) + "]", obj[i], add);
+        }
+      }
+    } else if (typeof obj == "object") {
+      // Serialize object item.
+      for (name in obj) {
+        _self.buildParams(prefix + "[" + name + "]", obj[ name ], add);
+      }
+    } else {
+      // Serialize scalar item.
+      add(prefix, obj);
+    }
   };
 
   // returning factory instance
