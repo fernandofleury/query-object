d58f4db55faa619182702274cfaf69a3d2258d2c
Fernando Fleury Gil <fernandofleuryg@gmail.com>
2017-10-20 23:38:12 +0200

    feat(parse): add parse method (converts queryStrings into objects)

---
+++ src/index.spec.js
@@ -0,0 +1,9 @@
+const queryObject = require('./index')
+
+describe('queryObject', () => {
+  describe('parse', () => {
+    it('should be defined', () => {
+      expect(queryObject.parse).toBeDefined()
+    })
+  })
+})
